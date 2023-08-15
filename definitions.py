bcaa3b2d8d10b56576c9594d555faf48ebab6c71
hamo <mfayad_cs@hotmail.com>
2023-08-15 18:28:40 -0300

    updated definitions and a simulation notebook

--- definitions.py
+++ definitions.py
@@ -1,10 +1,13 @@
 import math
+import time
+from random import randint, choices
+import string
 
 class Arena:
     def __init__(self, x=50, y=45):
         self.particles = []
         self.foods = []
-        self.grid = [[' ' for _ in range(x)] for _ in range(y)]
+        self.grid = [['   ' for _ in range(x)] for _ in range(y)]
         self.x = x
         self.y = y
 
@@ -14,21 +17,47 @@ class Arena:
     def add_food(self, Food):
         self.foods.append(Food)
 
-    def display(self):
+    def render(self):
+        self.grid = [['   ' for _ in range(self.x)] for _ in range(self.y)]
         for particle in self.particles:
-            self.grid[particle.position[1]][particle.position[0]] = 'P'
+            self.grid[particle.position[1]][particle.position[0]] = f'P{ particle.name,particle.position}'
         for food in self.foods:
-            self.grid[food.position[1]][food.position[0]] = 'F'
-
+            self.grid[food.position[1]][food.position[0]] = f'F{food.position}'
         for row in self.grid:
             print(''.join(row))
 
 
+    def random_position(self):
+        random_position = (randint(0,self.x), randint(0,self.y))
+        return random_position
+
+    def update_particles(self):
+        for particle in self.particles:
+            particle.move(self.foods)
+
+    def respawn_food(self):
+        random_position = self.random_position()
+        nutrition_value = randint(1,3)
+        food = Food(nutrition_value, random_position)
+        self.add_food(food)
+
+    def spawn_particles(self):
+        name = ''.join(choices(string.ascii_uppercase + string.digits, k=4))
+        random_position = self.random_position()
+        size = randint(1,5)
+        hunger = 10
+        self.add_particle(Particle(name, size, hunger, random_position))
+
+
+
 class Particle:
-    def __init__(self, size, hunger, position=(0,0)):
+    def __init__(self, name, size, hunger, position=(0,0)):
+        self.name = name
         self.size = size
         self.hunger = hunger
+        self.speed = int(5/size)
         self.position = position
+        
 
     def feed(self):
         self.hunger -= 1
@@ -38,21 +67,39 @@ class Particle:
 
     def move(self, foods):
         x,y = self.position
+        xspeed = yspeed = self.speed
         closest_food = min(foods, key=lambda food: math.sqrt((food.position[0] - x)**2 + (food.position[1] - y)**2))
         fx,fy = closest_food.position
-        if x > fx:
-            x -= 1
-        else:
-            x =+ 1
-        if y > fy:
-            y -= 1
-        else:
-            y += 1
-        self.position = (x,y)
-        
+        dx = x - fx 
+        dy = y - fy
+        distance = (dx**2 + dy**2)
+        if distance == 0:
+            return
+        if dx < xspeed:
+            xspeed = abs(dx)
+            # print(f'speed exceeds distance - new speed {xspeed}')
+        if dy < yspeed:
+            yspeed = abs(dy)
+            # print(f'speed exceeds distance - new speed {yspeed}')
 
-    # def seek_food(self):
-    #     if Arena.foods
+        # if dx > 0:
+        #     new_x = x - self.speed
+        # if dy > 0:
+        #     new_y = y - self.speed
+        # if dx < 0:
+        #     new_x = x + self.speed
+        # if dy < 0:
+        #     new_y = y + self.speed
+        # if dx == 0:
+        #     new_x = x
+        # if dy == 0:
+        #     new_y = y
+
+        new_x = x - int(xspeed) if dx > 0 else (x + int(xspeed) if dx < 0 else x)
+        new_y = y - int(yspeed) if dy > 0 else (y + int(yspeed) if dy < 0 else y)
+        new_position = (new_x, new_y)
+        self.position = new_position
+
 
 class Food:
     def __init__(self, nutrition, position=(0,0)):
@@ -60,29 +107,28 @@ class Food:
         self.position = position
 
 
-        
-particle1 = Particle(5, 10, (20,20))
-particle3 = Particle(5, 10, (45,40))
-particle2 = Particle(1, 1, (12,12))
-food1 = Food(2,(15,15))
-food2 = Food(2,(35,35))
+arena = Arena()
 
-arena1 = Arena()
+# for _ in range(8):
+#     arena.spawn_particles()
 
-arena1.add_food(food1)
-arena1.add_food(food2)
-arena1.add_particle(particle1)
-arena1.add_particle(particle2)
-arena1.add_particle(particle3)
-
-# arena1.display()
-print(particle1.position)
-
-for _ in range(10):
-    for particle in arena1.particles:
-        particle.move(arena1.foods)
+# for _ in range(10):
+#     arena.respawn_food()
 
 
-arena1.display()
-for particle in arena1.particles:
-    print(particle.position)
+arena.render()
+
+
+
+# while True:  # Run indefinitely for demonstration purposes
+#     # Update particle positions
+#     arena.update_particles()
+
+#     # Respawn food items
+#     arena.respawn_food()
+
+#     # Render the arena grid
+#     arena.render()
+
+#     # Introduce a delay (e.g., 1 second) to control the time interval between updates
+#     time.sleep(1)
