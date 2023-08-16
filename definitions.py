b4f5d34d36282656f0c3985c60d053e212d839b4
hamo <mfayad_cs@hotmail.com>
2023-08-15 23:40:43 -0300

    added consume

--- definitions.py
+++ definitions.py
@@ -3,10 +3,10 @@ from random import randint, choices
 import string
 
 class Arena:
-    def __init__(self, x=50, y=45):
+    def __init__(self, x=200, y=200):
         self.particles = []
         self.foods = []
-        self.grid = [['   ' for _ in range(x)] for _ in range(y)]
+        self.grid = [['   ' for _ in range(0,x)] for _ in range(0,y)]
         self.x = x
         self.y = y
 
@@ -17,14 +17,19 @@ class Arena:
         self.foods.append(Food)
 
     def render(self):
-        self.grid = [['   ' for _ in range(self.x)] for _ in range(self.y)]
+        self.grid = [['   ' for _ in range(0,self.x)] for _ in range(0,self.y)]
         for particle in self.particles:
-            self.grid[particle.position[1]][particle.position[0]] = f'P{ particle.name,particle.position}'
+            self.grid[particle.position[1]][particle.position[0]] = f'P{particle.name}'
         for food in self.foods:
-            self.grid[food.position[1]][food.position[0]] = f'F{food.position}'
+            self.grid[food.position[1]][food.position[0]] = f'F'
         for row in self.grid:
             print(''.join(row))
 
+    def check_collision(self, particle):
+        for food in self.foods:
+            if particle.position[0] == food.position[0] and particle.position[1] == food.position[1]:
+                self.foods.remove(food)
+                particle.hunger += food.nutrition
 
     def random_position(self):
         random_position = (randint(0,self.x), randint(0,self.y))
@@ -33,6 +38,7 @@ class Arena:
     def update_particles(self):
         for particle in self.particles:
             particle.move(self.foods)
+            self.check_collision(particle)
 
     def respawn_food(self):
         random_position = self.random_position()
@@ -66,12 +72,12 @@ class Particle:
 
     def move(self, foods):
         x,y = self.position
-        xspeed = yspeed = self.speed
+        xspeed = yspeed = self.speed/2
         closest_food = min(foods, key=lambda food: math.sqrt((food.position[0] - x)**2 + (food.position[1] - y)**2))
         fx,fy = closest_food.position
         dx = x - fx 
         dy = y - fy
-        distance = (dx**2 + dy**2)
+        distance = math.sqrt(dx**2 + dy**2)
         if distance == 0:
             return
         if dx < xspeed:
@@ -94,10 +100,11 @@ class Particle:
         # if dy == 0:
         #     new_y = y
 
-        new_x = x - int(xspeed) if dx > 0 else (x + int(xspeed) if dx < 0 else x)
-        new_y = y - int(yspeed) if dy > 0 else (y + int(yspeed) if dy < 0 else y)
-        new_position = (new_x, new_y)
+        new_x = x - xspeed if dx > 0 else (x + xspeed if dx < 0 else x)
+        new_y = y - yspeed if dy > 0 else (y + yspeed if dy < 0 else y)
+        new_position = (int(new_x), int(new_y))
         self.position = new_position
+        return new_position
 
 
 class Food:
